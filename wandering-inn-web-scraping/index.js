52ab6f2f12ec028bc9f1fadfdfbb09f8f43f7e50
Phil M. Harner <philmh@vt.edu>
2020-08-25 11:04:27 -0400

    Integrating the table of contents scraping with the individual chapter scraping.

--- wandering-inn-web-scraping/index.js
+++ wandering-inn-web-scraping/index.js
@@ -1,67 +1,50 @@
 const fs = require('fs');
 const axios = require('axios').default;
 const cheerio = require('cheerio');
-let data = [];
+let data = { chapters: [] };
 async function scrape() {
     const html = await axios.get('https://wanderinginn.com/table-of-contents/');
     const $ = cheerio.load(html.data);
-    $('p > a').each( (i, elem) => {
+    $('p > a').each((i, elem) => {
         const chapter = $(elem).text();
         const link = $(elem).attr('href');
-        data.push({chapter, link});
+        data.chapters.push({ chapter, link });
     });
 }
-async function scrapeChapter(chapData) {
-    const html = await axios.get(chapData.link);
+async function scrapeChapter(index) {
+    const html = await axios.get(data.chapters[i].link);
     const $ = cheerio.load(html.data);
-    chapData.time = $('.entry-date').attr('datetime');
-    console.log(chapData);
+    data.chapters[i].time = $('.entry-date').attr('datetime');
+
+    console.log(data.chapters[i]);
 }
 
 
 async function scrapeWanderingInn() {
-    scrape().then( () => {
-        data.forEach((chapData, index) => {
-            if (index > 3) throw 'Limiter reached';
-            scrapeChapter(chapData);
-        })
-        
-    })
-}
-// scrapeWanderingInn()
-// .then( () => {
-//     console.log(data);
-// }).catch( () => {
-//     console.log(data[0]);
-// })
+    await scrape()
+    .then(() => {
+        // data.chapters.some( (chapData, index) => { // TODO try array.some
+        //     if (index > 3) return true;
+        //     scrapeChapter(chapData);
+        // });
+        for (i = 0; i < 3; i++) {
+            scrapeChapter(i);
+        }
 
-data = [{
-    chapter: '1.00',
-    link: 'https://wanderinginn.com/2016/07/27/1-00/',
-    time: '2016-07-27T02:41:10+00:00'
-  },{
-    chapter: '1.01',
-    link: 'https://wanderinginn.com/2016/07/27/1-01/',
-    time: '2016-07-27T03:05:29+00:00'
-  },{
-    chapter: '1.03',
-    link: 'https://wanderinginn.com/2016/08/07/1-03/',
-    time: '2016-08-07T02:24:21+00:00'
-  },{
-    chapter: '1.02',
-    link: 'https://wanderinginn.com/2016/07/30/1-02/',
-    time: '2016-07-30T22:57:37+00:00'
-  }];
+    });
+};
 
-
-// JSON.stringify(data)
-let stringifiedData = [];
-data.forEach( (element) => {
-    stringifiedData.push(JSON.stringify(element));
+scrapeWanderingInn().then(() => {
+    const stringifiedData = JSON.stringify(data, null, 4);
+    console.log(stringifiedData);
+    fs.writeFileSync('output-data.json', stringifiedData);
 });
 
 
-console.log(stringifiedData);
-// stringifiedData.forEach( element => {
-    fs.writeFileSync('output-data.json', stringifiedData);
-// });
+/* Data for dataset
+[{word1, count}, {word2, count}, .....]
+total words per chapter | total words
+chapter date
+total comments
+number of non-avatar images
+*/
